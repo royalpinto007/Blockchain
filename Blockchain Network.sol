546708fd81b44dc3099af811332bcf0d94bcddeb
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-17 10:13:18 +0530

    Create Blockchain Network.sol

---
+++ Blockchain Network.sol
@@ -0,0 +1,11 @@
+// Project : Blockchain Network
+// Problem Statement: You are an employee of some company and have been asked to create a Blockchain network to demonstrate the working of Blockchain.
+
+// Steps to Perform:
+// Step 1: Visit https://blockchaindemo.io/ and create blocks for the existing peer. Enter data in the data field, and click Add New Block.
+// Step 2: Create another peer and blocks.
+// o	Click on Add Peer button to add a new peer
+// o	Select the new peer created
+// o	Enter data in the data field, and click Add New Block to add blocks to the new peer
+// Step 3: Connect the newly created peer by clicking on the connect symbol.
+// Step 4: Similarly, add peers and blocks and create a Blockchain network.
