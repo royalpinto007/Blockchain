57b307975b36456451822b875aebaa121538d285
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-17 10:14:31 +0530

    Create Blockchain on Multichain.cpp

---
+++ Blockchain on Multichain.cpp
@@ -0,0 +1,19 @@
+// Blockchain on Multichain 
+// Create Private Multichain Blockchain 
+
+// Description: Create a private Blockchain using Multichain platform with some modifications in parameters in params.dat file.
+
+// Steps to perform: 
+// •	Create a Multichain
+// •	Modify the parameters in the params.dat file:
+
+// 1.	Basic chain parameters
+//    Provide chain description = “you can provide description to your blockchain”
+// 2.	Global permissions
+//    Set “Anyone-can-connect= true”
+// 3. Native Blockchain currency
+//    Set “Initial-block-reward= 286730“(Set any value of your choice)
+//    Set “Minimum relay fee= 130”
+// •	Connect to the Blockchain
+// •	Create assets
+// •	Transfer assets to the peer
