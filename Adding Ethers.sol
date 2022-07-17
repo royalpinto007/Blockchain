a8ecf7fff8c562db376315fd5a98b711749ab5ef
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-17 10:11:52 +0530

    Create Adding Ethers.sol

---
+++ Adding Ethers.sol
@@ -0,0 +1,9 @@
+// Ethereum 
+// MetaMask Ether Faucet
+
+// Description: Create a MetaMask account to add Ethers to your MetaMask account using Ether Faucet.
+
+// Steps to perform: 
+// •	Create a  MetaMask account
+// •	Connect to Ropsten Test Network
+// •	Add Ethers to your account
