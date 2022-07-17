51dcde63098a489170b8c4d9e6f812bc9905e742
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-17 10:22:52 +0530

    Create Generate a Bitcoin Wallet.sol

---
+++ Generate a Bitcoin Wallet.sol
@@ -0,0 +1,13 @@
+// Project: Generate a Bitcoin Wallet
+
+// Problem Statement: You are an employee of some company and have been asked to create a Bitcoin wallet for the company. You must perform the following actions:
+// ●	Generate a single wallet
+// ●	Generate a paper wallet with a passphrase
+// ●	Check wallet details
+
+// Steps to Perform:
+// Step 1: Visit https://www.bitaddress.org/  
+// Step 2: Hover over the web page to generate a Bitcoin wallet address
+// Step 3: Generate a single wallet
+// Step 4: Generate a paper wallet with a passphrase and download it
+// Step 5: Check wallet details by inserting the private key of the single wallet
